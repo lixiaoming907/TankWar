c998f21a97c192d65fe05928b8f7dff483f77e4a
小明大大 <907277231@qq.com>
2016-10-07 23:52:02 +0800

    新增计分面板

---
+++ Assets/Scripts/ScoreCountWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreCountWindow : MonoBehaviour
+{
+
+    public GameObject scoreCountPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Tab)) //显示分数面板
+	    {
+            scoreCountPanel.SetActive(true);
+        }
+	    if (Input.GetKeyUp(KeyCode.Tab)) //隐藏分数面板
+	    {
+            scoreCountPanel.SetActive(false);
+        }
+	}
+}
