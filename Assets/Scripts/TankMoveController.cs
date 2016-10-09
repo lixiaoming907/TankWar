a44519a67cbc8600530b201ebe9edbc9fe404eb4
lixiaoming907 <907277231@qq.com>
2016-10-09 20:00:07 +0800

    新增根据鼠标位置旋转炮口

--- Assets/Scripts/TankMoveController.cs
+++ Assets/Scripts/TankMoveController.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 using UnityEngine.Networking;
 
 public class TankMoveController : NetworkBehaviour
 {
     public float moveSpeed = 10;
     public float rotateSpeed = 5;
-
-    public GameObject playerController;
 
     private Rigidbody rigid;
     [HideInInspector]
@@ -17,6 +16,11 @@ public class TankMoveController : NetworkBehaviour
     public AudioClip engineWalkClip; //移动声音
 
     public AudioSource audioSource;
+
+    public GameObject tankAbove; //坦克上半身
+    private Vector3 mousePositionOffer; //用来确定鼠标位置
+    private RaycastHit hitInfo;
+    private Ray ray;
 
     private bool _isMove;
     [HideInInspector]
@@ -60,6 +64,23 @@ public class TankMoveController : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isLocalPlayer)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo, 1000))
+            {
+                Vector3 playerToMouse = hitInfo.point - transform.position;
+
+                playerToMouse.y = 0f; //不会向上
+
+                Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
+                //传入向量playerToMouse，返回一个四元数类型的newRotation
+
+                //myPlayerRigidbody.MoveRotation(newRotatation); //人物朝向向量方向
+                tankAbove.transform.rotation = Quaternion.Lerp(tankAbove.transform.rotation, newRotatation, 0.03f);
+            }
+        }
 
     }
 
